b6122072395f1d2263b4dd39257dbb192b5e7c63
haslozik <wojciechperlinskitpel@gmail.com>
2021-07-09 11:56:34 +0200

    chart added

--- app.js
+++ app.js
@@ -30,11 +30,46 @@ function showTime(){
 
 showTime();
 
-// /*chart*/
-// const config = {
-//     type: 'line',
-//     data,
-//     options: {}
-// };
+// charts
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Jun 21', 'Jun 22', 'Jun 23', 'Jun 24', 'Jun 25', 'Jun 26'],
+        datasets: [{
+            label: '# of Votes',
+            data: [0.5, 1, 1, 2, 4, 6],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 2
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        },
+        plugins: {
+            legend: {
+              display: false
+            }
+          }
+    }
+});
 
 
